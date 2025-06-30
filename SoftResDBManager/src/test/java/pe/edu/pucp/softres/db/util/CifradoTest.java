8722f379ec0f68bfd5081267bcdd51921d8e4db5
Fernando5603 <fjrlt2003@gmail.com>
2025-06-29 22:32:52 -0500

    namas le borre los datos del jdbc.properties gaa

--- SoftResDBManager/src/test/java/pe/edu/pucp/softres/db/util/CifradoTest.java
+++ SoftResDBManager/src/test/java/pe/edu/pucp/softres/db/util/CifradoTest.java
@@ -35,18 +35,18 @@ public class CifradoTest {
         String resultado_esperado = "bryan123";
         assertEquals(resultado, resultado_esperado);
     }
-    @Test
-    public void testCifrar() {
-        System.out.println("cifrarMD5");
-        String texto = "fernandor72802756";
-        String resultado = Cifrado.cifrarMD5(texto);
-        System.out.println(resultado);
-    }
-    @Test
-    public void testDescifrar() {
-        System.out.println("descifrarMD5");
-        String texto = "YGOE2OvoI245rjz/OrNSMg==";
-        String resultado = Cifrado.descifrarMD5(texto);
-        System.out.println(resultado);
-    }
+//    @Test
+//    public void testCifrar() {
+//        System.out.println("cifrarMD5");
+//        String texto = "";
+//        String resultado = Cifrado.cifrarMD5(texto);
+//        System.out.println(resultado);
+//    }
+//    @Test
+//    public void testDescifrar() {
+//        System.out.println("descifrarMD5");
+//        String texto = "YGOE2OvoI245rjz/OrNSMg==";
+//        String resultado = Cifrado.descifrarMD5(texto);
+//        System.out.println(resultado);
+//    }
 }
